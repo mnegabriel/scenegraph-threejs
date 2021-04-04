c57d869b9ba8cc442201310583228e387df676a8
Gabriel Dantas <gabriel.dantas@catskillet.com>
2021-04-04 17:02:39 -0300

    earth rotating around the sun

--- js/classes/AstralBodyGeometry.js
+++ js/classes/AstralBodyGeometry.js
@@ -5,11 +5,31 @@ export default class {
 	_WIDTH_SEGMENTS = 6;
 	_HEIGHT_SEGMENTS = 6;
 
-	constructor() {
+	constructor(materialObject) {
+		this._materialObject = materialObject
+
+		this._executeCreation()
+	}
+	
+	_executeCreation() {
+		this._createPhongMaterial()
+		this._createGeometry()
+		this._createAstralBody()
+	}
+	
+	_createPhongMaterial() {
+		this.material = new THREE.MeshPhongMaterial(this._materialObject);
+	}
+	
+	_createGeometry() {
 		this.sphere = new THREE.SphereGeometry(
 			this._RADIUS,
 			this._WIDTH_SEGMENTS,
 			this._HEIGHT_SEGMENTS
-		);
+		);		
+	}
+
+	_createAstralBody() {
+		this.astralBody = new THREE.Mesh(this.sphere, this.material);
 	}
 }
--- js/main.js
+++ js/main.js
@@ -7,32 +7,38 @@ import AstralBodyGeometry from "./classes/AstralBodyGeometry.js";
 import SceneLighting from "./classes/SceneLighting.js";
 
 function main() {
-    initLog();
-    
-    const canvas = document.querySelector("canvas#c");
-    const setup = new InitialSetupper(canvas);
-    
-    const astralBodyGeometry = new AstralBodyGeometry().sphere;
-    
-    const sumMaterial = new THREE.MeshPhongMaterial({ emissive: 0xFFFF00 });
-    const sunMesh = new THREE.Mesh(astralBodyGeometry, sumMaterial);
-    
-    sunMesh.scale.set(5, 5, 5);
-    
-    const light = new SceneLighting().light;
-    
-    const objectsOnScene = [ sunMesh];    
-    setup.scene.add(light,...objectsOnScene);    
+	initLog();
 
-    function animate(timeElapsed) {
-        timeElapsed /= 1000
+	const canvas = document.querySelector("canvas#c");
+	const setup = new InitialSetupper(canvas);
 
-        sunMesh.rotation.y = timeElapsed
+	const light = new SceneLighting().light;
 
-        setup.renderer.render(setup.scene, setup.camera)
-        requestAnimationFrame(animate)
-    }
+	const sun = new AstralBodyGeometry({ emissive: 0xffff00 }).astralBody;
+	const earth = new AstralBodyGeometry({
+		color: 0x2233ff,
+		emissive: 0x112244,
+	}).astralBody;
 
-    animate()
+	sun.scale.set(5, 5, 5);
+	earth.position.x = 10;
+    
+    const solarSystem = new THREE.Object3D();
+		const contentOfSolarSystem = [sun, earth];
+		solarSystem.add(...contentOfSolarSystem);
+    
+	const objectsOnScene = [sun, earth, solarSystem];
+	setup.scene.add(light, solarSystem);
+
+	function animate(timeElapsed) {
+		timeElapsed /= 1000;
+
+		objectsOnScene.forEach((obj) => (obj.rotation.y = timeElapsed));
+
+		setup.renderer.render(setup.scene, setup.camera);
+		requestAnimationFrame(animate);
+	}
+
+	animate();
 }
-main()
+main();
